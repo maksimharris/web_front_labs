9c37556bf3813be7d13cea91380a3836dd31b512
Максим Писарев <maksim.pisarev.1986@mail.ru>
2025-05-14 01:00:38 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,91 @@
+const books = [
+    {
+        author:'Эдит Ева Эгер',
+        name:'Выбор. О свободе и внутренней силе человека',
+        publicationYear: 2019,
+        price: 760,
+        bestseller: true, 
+        availability: false
+    },
+    {
+        author:'Али Хейзелвуд',
+        name:'Гипотеза любви',
+        publicationYear: 2021,
+        price: 874,
+        bestseller: false, 
+        availability: false
+    },
+    {
+        author:'Александр Куприн',
+        name:'Гранатовый браслет',
+        publicationYear: 1958,
+        price: 1090,
+        bestseller: true, 
+        availability: true,
+    },
+    {
+        author:'Фёдор Достоевский',
+        name:'Игрок',
+        publicationYear: 1866,
+        price: 1350,
+        bestseller: false, 
+        availability: true
+    },
+    {
+        author:'Фредрик Бакман',
+        name:'Тревожные люди',
+        publicationYear: 2020,
+        price: 625,
+        bestseller: false, 
+        availability: false
+    },
+    {
+        author:'Донна Тартт',
+        name:'Щегол',
+        publicationYear: 2024,
+        price: 1050,
+        bestseller: true, 
+        availability: true
+    },
+    {
+        author:'Айн Рэнд',
+        name:'Атлант расправил плечи',
+        publicationYear: 2021,
+        price: 720,
+        bestseller: true, 
+        availability: false
+    },
+    {
+        author:'Алекс Михаэлидес',
+        name:'Безмолвный пациент',
+        publicationYear: 2025,
+        price: 900,
+        bestseller: false, 
+        availability: true
+    },
+    {
+        author:'Алекс Михаэлидес',
+        name:'Девы',
+        publicationYear: 2025,
+        price: 800,
+        bestseller: true, 
+        availability: false
+    },
+    {
+        author:'Джон Маррс',
+        name:'The One. Единственный',
+        publicationYear: 2024,
+        price: 795,
+        bestseller: false, 
+        availability: false
+    },
+    {
+        author:'Эл Моргот',
+        name:'Злодейский путь!.. Том 6',
+        publicationYear: 2023,
+        price: 820,
+        bestseller: true, 
+        availability: true
+    }
+
+]
